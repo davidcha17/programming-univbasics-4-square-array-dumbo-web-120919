4c56436442fd89e65611f1e945b34b8bdecdb21d
David Cha <david.cha44@qmail.cuny.edu>
2019-11-07 18:50:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   new_arr = []
-    array.each do |num|
+  counter = 0
+    while array[counter] do
       puts num ** 2
       new_arr << num ** 2
+      counter += 1
     end
 end
